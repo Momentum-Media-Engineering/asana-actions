1fe284d6b0ebd1555cfbb3ebcee4e81c4cbbf4a1
yizunyuan <yuan.yizun@momentummedia.com.au>
2025-07-21 17:59:01 +1000

    feat: init reviewer update

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ import axios from 'axios';
 
 async function run() {
   try {
+    core.info('Starting the GitHub Asana Comment Action!');
+    const context = github.context;
+
+    //check the event action
+    const eventAction = context.payload.action;
+    const eventName = context.eventName;
+
+    if (eventName === 'pull_request') {
+       core.info(`Pull request event with action: ${eventAction || 'unknown'}`);
+      if (eventAction === 'review_requested') {
+        const requestedReviewer = context.payload.requested_reviewer?.login;
+        core.info(`Review requested for: ${requestedReviewer}`);
+      }
+    }
+
     const taskId = core.getInput('task_id');
     const comment = core.getInput('comment');
     const htmlText = core.getInput('html_text');
@@ -11,8 +26,13 @@ async function run() {
     const dynamic = core.getInput('dynamic');
     const apiUrl = core.getInput('api_url');
 
+    core.info(`Task ID: ${taskId}`);
+    core.info(`Comment: ${comment}`);
+    core.info(`HTML Text: ${htmlText}`);
+    core.info(`Dynamic: ${dynamic}`);
+    core.info(`API URL: ${apiUrl}`);
 
-    const context = github.context;
+
     const pullRequest = context.payload.pull_request;
     const pullRequestDescription = pullRequest?.body;
     const pullRequestAuthor = pullRequest?.user?.login;
@@ -21,12 +41,24 @@ async function run() {
     const pullRequestURL = pullRequest?.html_url;
     const pullRequestState = pullRequest?.state;
     const pullRequestMerged = pullRequest?.merged || false;
+    const requestedReviewer = context.payload.requested_reviewer?.login;
+
+    core.info(`Pull Request Description: ${pullRequestDescription}`);
+    core.info(`Pull Request Author: ${pullRequestAuthor}`);
+    core.info(`Pull Request ID: ${pullRequestId}`);
+    core.info(`Pull Request Name: ${pullRequestName}`);
+    core.info(`Pull Request URL: ${pullRequestURL}`);
+    core.info(`Pull Request State: ${pullRequestState}`);
+    core.info(`Pull Request Merged: ${pullRequestMerged}`);
+    core.info(`Requested Reviewer: ${requestedReviewer}`);
 
     const response = await axios.post(apiUrl, {
       task_id: taskId,
       comment: comment,
       html_text: htmlText,
       dynamic: dynamic,
+      pullRequestEventAction: eventAction,
+      pullRequestEventName: eventName,
       pullRequestDescription: pullRequestDescription,
       pullRequestAuthor: pullRequestAuthor,
       pullRequestId: pullRequestId,
@@ -34,6 +66,8 @@ async function run() {
       pullRequestURL: pullRequestURL,
       pullRequestState: pullRequestState,
       pullRequestMerged: pullRequestMerged,
+      requestedReviewer: requestedReviewer,
+
     }, {
       headers: {
         'Content-Type': 'application/json',
